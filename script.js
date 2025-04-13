c9faf27de9e815eff49515acce72c7d594861f92
Max Minihan <maxminihan@gmail.com>
2025-04-13 16:24:05 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function showMessage() {
+    alert("Hello " + yourName + "! If you like my website, please contact me on 07887598461 or maxminihan@gmail.com to say hello.");
+  }
+  var yourName = prompt("What is your name?");
+  var myName = "Max";
+
+  alert("My name is " + myName + " Welcome to my website " + yourName);
